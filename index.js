b95f42d677ae61ce24aee5d76c9ce91d902f0168
Kumar Lokesh Singh <38185323+klsingh@users.noreply.github.com>
2020-05-23 21:57:41 +0530

    created new file

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hello World')
+//const square ={
+//    area : (a) => (a*a),
+//    perimeter : (a) => (4*a)
+//}
+
+const square = require('./Square.js');
+
+const calsquare = (a) =>{
+    console.log(`the value of a is ${a} and the area is`  +square.area(a));
+    console.log(`the value of a is ${a} and the perimeter is`  +square.perimeter(a));
+}
+
+console.log(__filename);
+console.log(__dirname);
+
+calsquare(5);
